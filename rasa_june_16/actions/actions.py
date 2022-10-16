ee5c835d1ecf870099a2efeb94fce23caff79a32
DESKTOP-QJH7DRA\S H K <ajwad.din@funavry.com>
2022-10-16 15:15:31 +0500

    Update actions.py

--- rasa_june_16/actions/actions.py
+++ rasa_june_16/actions/actions.py
@@ -1846,7 +1846,7 @@ class ActionGetShoppingList(Action):
             day = ((datetime.datetime.today() - user_meals[0]['day_created']).days + 1)
             day_number = 7 - day
             if day <= 7:
-                dispatcher.utter_message(text = f"You already are on a {diet_type} diet plan and currently on day number{day_number}\n\
+                dispatcher.utter_message(text = f"You already are on a {diet_type} diet plan and currently on day number {day_number}\n\
                     Say things like “give me a breakfast plan”. And I will be able to give you your meal plan.\n\
                     If you want to change the diet type then try typing something like \'change my diet type from keto to low-carb\' and I'll give you a new diet plan.")
                 return [SlotSet("diet_type", None)]
